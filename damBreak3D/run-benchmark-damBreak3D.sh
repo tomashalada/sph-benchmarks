239ca07a3ba5619109e8aefac7d35dcb780112c0
tomashalada <halada.tms@gmail.com>
2024-06-15 23:16:48 +0200

    Adjusted resolutions.

--- damBreak3D/run-benchmark-damBreak3D.sh
+++ damBreak3D/run-benchmark-damBreak3D.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 ## benchmark setup
-benchmarkName="local-test"
+benchmarkName="dambreak3D"
 samples=1
-resolutions="0.02"
+#resolutions="0.02 0.015 0.013 0.012 0.011 0.009 0.0095 0.0085 0.008 0.0075 0.007 0.0065 0.006"
+
 
 ## results folder
 resultsFolder="results_"$benchmarkName
